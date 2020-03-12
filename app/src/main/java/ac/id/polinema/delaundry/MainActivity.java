7981d9fec1a5f961b9adff19b251994a2e44700c
HattaMaulana <mahatta.maulana@gmail.com>
2020-03-12 11:31:32 +0700

    create splash screen

--- app/src/main/java/ac/id/polinema/delaundry/MainActivity.java
+++ app/src/main/java/ac/id/polinema/delaundry/MainActivity.java
@@ -2,13 +2,36 @@ package ac.id.polinema.delaundry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Runnable runAsync = new Runnable() {
+        @Override
+        public void run() {
+            // TODO : Masukkan Target Activity
+            Intent intent = new Intent();
+            startActivity(intent);
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setStatusBarTransparent();
+
+        new Handler().postDelayed(runAsync, 3000);
+    }
+
+    private void setStatusBarTransparent() {
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
     }
 }
