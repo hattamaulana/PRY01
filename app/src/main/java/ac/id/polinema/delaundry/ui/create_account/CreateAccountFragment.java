8b56bb5ef788d3d11c92d88d2843081961d1855d
HattaMaulana <mahatta.maulana@gmail.com>
2020-03-19 12:07:30 +0700

    Add Form Validation and View Binding at Authentication Form

--- app/src/main/java/ac/id/polinema/delaundry/ui/create_account/CreateAccountFragment.java
+++ app/src/main/java/ac/id/polinema/delaundry/ui/create_account/CreateAccountFragment.java
@@ -4,17 +4,84 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import ac.id.polinema.delaundry.R;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
 
-public class CreateAccountFragment extends Fragment {
+import java.util.List;
+
+import ac.id.polinema.delaundry.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static androidx.navigation.Navigation.findNavController;
+
+public class CreateAccountFragment extends Fragment implements Validator.ValidationListener {
+
+    private Validator validator;
+
+    @NotEmpty
+    @BindView(R.id.edt_name)
+    public EditText name;
+
+    @NotEmpty
+    @BindView(R.id.edt_address)
+    public EditText address;
+
+    @NotEmpty
+    @Password(min = 8, scheme = Password.Scheme.ALPHA_NUMERIC_MIXED_CASE_SYMBOLS)
+    @BindView(R.id.edt_password)
+    public EditText password;
+
+    @NotEmpty
+    @ConfirmPassword
+    @BindView(R.id.edt_confirm_password)
+    public EditText confirmPassword;
+
+    @OnClick(R.id.btn_register) void register() {
+        validator.validate();
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_create_account, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+        ButterKnife.bind(this, getView());
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        findNavController(getView()).navigate(R.id.createAccountToHome);
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(getContext());
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
--- app/src/main/java/ac/id/polinema/delaundry/ui/register/RegisterFragment.java
+++ app/src/main/java/ac/id/polinema/delaundry/ui/register/RegisterFragment.java
@@ -4,17 +4,68 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import ac.id.polinema.delaundry.R;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 
-public class RegisterFragment extends Fragment {
+import java.util.List;
+
+import ac.id.polinema.delaundry.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static androidx.navigation.Navigation.findNavController;
+
+public class RegisterFragment extends Fragment implements Validator.ValidationListener {
+
+    private Validator validator;
+
+    @NotEmpty
+    @BindView(R.id.edt_nohandphone)
+    public EditText noHandphone;
+
+    @OnClick(R.id.btn_register) void submit() {
+        validator.validate();
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+        ButterKnife.bind(this, getView());
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        findNavController(getView()).navigate(R.id.registerToCreateAccount);
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(getContext());
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
